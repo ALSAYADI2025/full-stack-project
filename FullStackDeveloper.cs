56700ed0bc8243bfb35846a7046d62f39057a3f9
ALSAYADI2025 <engabdulfattahalsayadi2023@gmail.com>
2025-02-24 09:03:31 +0300

    Create FullStackDeveloper.cs
    Create FullStackDeveloper.cs

---
+++ FullStackDeveloper.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FullStackDeveloper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
